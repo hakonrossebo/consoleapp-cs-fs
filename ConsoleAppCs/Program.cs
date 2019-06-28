d34d2214e53b40581c74bbaa3806a6957776ff1f
Rossebø, Håkon <Hakon.Rossebo@mhwirth.com>
2019-06-28 13:03:07 +0200

    Initial commit

---
+++ ConsoleAppCs/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var listOfIntegers = new List<int> { 1, 2, 3, 4, 10, 20 };
+
+            //Standard without using Linq
+            var newList = new List<int>();
+            foreach (var i in listOfIntegers)
+            {
+                var n = i * 2;
+                newList.Add(n);
+            }
+
+            // Using Linq to map/select over items in a collection 
+            var newList2 =
+                listOfIntegers.Select(number => number * 2);
+
+            var newList3 =
+                listOfIntegers.Select(TimesTwo);
+
+            var newList4 =
+                listOfIntegers.Select(x => MultiArgumentFunction(10, 20, x));
+
+            var newListAsStrings =
+                listOfIntegers.Select(tall => "Result is " + (tall * 2).ToString());
+
+            // Using Aggregate
+            var newListOfOddsSummarized =
+                listOfIntegers
+                    .Where(x => x % 2 != 0)
+                    .Sum();
+
+            var newListOfOddsSummarizedWithAccumulateFunction =
+                listOfIntegers
+                    .Where(x => x % 2 != 0)
+                    .Aggregate(0, AccumulateNumbers);
+
+            var newListOfOddsSummarizedWithAccumulateLambda =
+                listOfIntegers
+                    .Where(x => x % 2 != 0)
+                    .Aggregate(0, (accumulated, current) => accumulated + current);
+
+
+            // Writing collection to console
+            foreach (var i in newList3)
+            {
+                Console.WriteLine($"The values are {i}");
+            }
+        }
+
+        private static int TimesTwo(int aNumber)
+        {
+            return aNumber * 2;
+        }
+
+        private static int AccumulateNumbers(int accumulated, int current)
+        {
+            return accumulated + current;
+        }
+        private static int MultiArgumentFunction(int existingNumber1, int existingNumber2, int aNumber)
+        {
+            return existingNumber1 + existingNumber2 + aNumber * 2;
+        }
+    }
+}
---
+++ ConsoleAppFs/Program.fs
@@ -0,0 +1,45 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    let listOfIntegers = [ 1; 2; 3; 4; 10; 20 ]
+
+    let timesTwo number = number * 2
+    
+    let multiArgumentFunction existingNumber1 existingNumber2 number = existingNumber1 + existingNumber2 +  number * 2
+
+    let accumulateNumbers accumulated current = accumulated + current
+        
+
+    let newList = listOfIntegers 
+                    |> List.map (fun x -> x * 2)
+
+    let newList2 = listOfIntegers 
+                    |> List.map timesTwo
+    
+    let newList3 = 
+        let prefilledMutiArgumentFunction = multiArgumentFunction 10 20
+        listOfIntegers 
+        |> List.map prefilledMutiArgumentFunction
+
+    let newListOfOddsSummarizedWithAccumulateLambda =
+        listOfIntegers
+        |> List.filter (fun x -> x % 2 = 0)
+        |> List.fold (fun acc current -> acc + current) 0
+    
+    let newListOfOddsSummarizedWithAccumulateFunction =
+        listOfIntegers
+        |> List.filter (fun x -> x % 2 = 0)
+        |> List.fold accumulateNumbers 0
+
+    
+    newList2
+    |> List.iter (printfn "Numbers are: %d")
+
+    printfn "Sum is: %d" newListOfOddsSummarizedWithAccumulateLambda
+
+    0 // return an integer exit code
